6fc710a21becf2b8654f94513dd139b109d906df
Kamilovkz <001kamilov@gmail.com>
2023-05-13 13:28:07 +0600

    dockerize project

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,5 +1,5 @@
 command = '/home/promelon/Desktop/ui-craft/env/bin/gunicorn'
 pythonpath  = '/home/promelon/Desktop/ui-craft/uiproject/core'
-bind = '192.168.0.102:8000'
+bind = '192.168.0.102:7000'
 workers = 3
 
